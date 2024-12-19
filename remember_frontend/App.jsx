57d5223a02647e04aec62b00cff0c922d59a4a30
unknown <rikthmeowhumayun@gmail.com>
2024-12-19 15:13:45 +0600

    ngrok tunnel not work i want to die

--- remember_frontend/App.jsx
+++ remember_frontend/App.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react"
 import "./App.css"
+import { Text, View } from "react-native"
+import * as Notifications from "expo-notifications"
+
 
 const App = () => {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlayAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false
+    })
+  }) 
+
+  Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Look at that notification',
+      body: "I'm so proud of myself!",
+    },
+    trigger: { seconds: 5, repeats: true },
+  });
   const [color, setColor] = useState('red')
   const [colorcount, setcolorCount] = useState(0)
   const colorArray = ['blue', 'green', 'red']
@@ -13,14 +31,15 @@ const App = () => {
     setcolorCount(colorcount+1)
     if (colorcount >= 2) setcolorCount(0)
   }
-
+  
   return (
-    
+    <View className = {color}>
     <div>
-      <button onClick = {colorChanger} className = {color}>
-        Good
+      <button onClick = {colorChanger} >
+        <Text className = {color}>Good</Text>
       </button>
     </div>
+    </View>
   )
 }
 export default App
